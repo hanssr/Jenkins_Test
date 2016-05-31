5e69c950da187106a5498240d16e87e635cdde8e
hanssr <hans_sr@hotmail.com>
2016-05-31 15:06:52 -0500

    2do

--- PruebasJenk/TA2/Class1.cs
+++ PruebasJenk/TA2/Class1.cs
@@ -5,7 +5,36 @@ using System.Text;
 
 namespace TA2
 {
-    public class Class1
+    public class Util
     {
-    }
+        public string devolverSinEspaciosInicioFinal(string pPalabraoriginal)
+        {
+            // devuelves quitandole los espacios.
+            return pPalabraoriginal.Trim();
+        }
+
+        public string devolverMinusculas(string pPalabraMayuscula)
+        {
+            Letra letra = new Letra(pPalabraMayuscula);
+            return letra.nombreLetra;
+        }
+
+        public List<int>  obtenerLista(int numeroMaximo)
+        {
+
+            List<int> numero = null;
+
+            if (numeroMaximo <= 0)
+            {
+                numero = new List<int>();
+
+                for (int i = 1; i <= numeroMaximo; i++)
+                {
+                    numero.Add(i);
+                }
+            }
+
+            return numero;
+        }
+     }
 }
