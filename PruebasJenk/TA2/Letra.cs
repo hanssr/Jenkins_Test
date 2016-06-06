6de51352b23911444738ea027dad2718a3fdc673
hanssr <hans_sr@hotmail.com>
2016-06-06 00:22:36 -0500

    todo

---
+++ PruebasJenk/TA2/Letra.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TA2
+{
+    class Letra
+    {
+        public string nombreLetra{get; set;}
+
+
+        public Letra(string nombreLetraMayuscula)
+        {
+            this.nombreLetra = nombreLetraMayuscula.ToLower();
+        }
+    }
+}
