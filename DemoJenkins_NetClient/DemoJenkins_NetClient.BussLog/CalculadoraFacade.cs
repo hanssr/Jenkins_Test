49b4e3cc57155ab1e29e24e55f3bfebd4a2f097c
hanssr <hans_sr@hotmail.com>
2016-06-06 00:03:21 -0500

    Commit correcion

--- DemoJenkins_NetClient/DemoJenkins_NetClient.BussLog/CalculadoraFacade.cs
+++ DemoJenkins_NetClient/DemoJenkins_NetClient.BussLog/CalculadoraFacade.cs
@@ -1,40 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
-namespace DemoJenkins_NetClient.BussLog 
+namespace DemoJenkins_NetClient.BussLog
 {
-    
+
     public class CalculadoraFacade
     {
-        private static DemoJenkins_JavaServer.CalculadoraWSClient ws = null;
+        /// private static DemoJenkins_JavaServer.CalculadoraWSClient ws = null;
 
         private static void Initialize()
         {
-            ws = new DemoJenkins_JavaServer.CalculadoraWSClient();
+            // ws = new DemoJenkins_JavaServer.CalculadoraWSClient();
         }
         public static int Suma(int x, int y)
         {
-            Initialize();
-            return ws.Suma(x, y);
+            //Initialize();
+            //return ws.Suma(x, y);
+
+            return x + y;
         }
 
         public static int Resta(int x, int y)
         {
-            Initialize();
-            return ws.Resta(x, y);
+            // Initialize();
+            // return ws.Resta(x, y);
+            return x - y;
         }
 
         public static int Division(int x, int y)
         {
-            
-            return x/y;
+
+            return x / y;
         }
 
         public static bool checkinputs(int a, int b)
         {
             bool condition = true;
-            
+
             if (a == b)
             {
                 condition = false;
@@ -58,13 +62,32 @@ namespace DemoJenkins_NetClient.BussLog
             Letra letra = new Letra(pPalabraMayuscula);
             return letra.letra;
         }
+        public static string devolverMayusculas(string pPalabra)
+        {
+            return pPalabra.ToUpper();
+        }
 
-        public static List<int>  obtenerLista(int numeroMaximo)
+        public static int cantidadLetras(string pPalabra)
+        {
+            return pPalabra.Length;
+        }
+
+        public static string seEncontroenTexto(string textoOri, string textBuscar)
+        {
+            return textoOri.Contains(textBuscar) ? textBuscar : string.Empty;
+        }
+
+        public static string concatenar(string pPalabra1, string pPalabra2)
+        {
+            return pPalabra1 + pPalabra2;
+        }
+
+        public static List<int> obtenerLista(int numeroMaximo)
         {
 
             List<int> numero = null;
 
-            if (numeroMaximo <= 0)
+            if (numeroMaximo > 0 && numeroMaximo < 10000000)
             {
                 numero = new List<int>();
 
@@ -76,6 +99,105 @@ namespace DemoJenkins_NetClient.BussLog
 
             return numero;
         }
-     }
+
+        public static int SumaDeTextoDosNumeros()
+        {
+
+            //List<int> numeros = new List<int>();
+            //var leer = new StreamReader("archivo1.txt");
+
+
+            //int numero1 = Convert.ToInt32(leer.ReadLine());
+            //int numero2 = Convert.ToInt32(leer.ReadLine());
+            //int respuesta = Convert.ToInt32(leer.ReadLine()); 
+
+
+
+            //foreach (int num in numeros)
+            //{
+            //    suma += num;
+            //}
+
+            //return suma;
+            return 0;
+        }
+
+        public static string concatenarTextodeTexto()
+        {
+
+            List<string> letras = new List<string>();
+            var leer = new StreamReader("archivo2.txt");
+
+            while (!leer.EndOfStream)
+            {
+                letras.Add(leer.ReadLine());
+            }
+
+            string texto = string.Empty;
+
+            foreach (var let in letras)
+            {
+                texto = texto + " " + let;
+            }
+
+            return texto;
+        }
+
+        public static int cantidadLetrasdeTexto()
+        {
+            var leer = new StreamReader("archivo3.txt");
+
+            string texto = string.Empty;
+
+            while (!leer.EndOfStream)
+            {
+                texto = texto + leer.ReadLine();
+            }
+
+            return texto.Length;
+        }
+
+        public static string aMayusculadeTexto()
+        {
+            var leer = new StreamReader("archivo4.txt");
+
+            string texto = string.Empty;
+
+            while (!leer.EndOfStream)
+            {
+                texto = texto + " " + leer.ReadLine();
+            }
+
+            return texto.ToUpper();
+        }
+
+        public static string aMinusculadeTexto()
+        {
+            var leer = new StreamReader("archivo5.txt");
+
+            string texto = string.Empty;
+
+            while (!leer.EndOfStream)
+            {
+                texto = texto + " " + leer.ReadLine();
+            }
+
+            return texto.ToLower();
+        }
+
+        public static bool seEncontroenTexto(string palabra)
+        {
+            var leer = new StreamReader("archivo5.txt");
+
+            string texto = string.Empty;
+
+            while (!leer.EndOfStream)
+            {
+                texto = texto + leer.ReadLine();
+            }
+
+            return texto.Contains(palabra);
+        }
+    }
 }
 
--- DemoJenkins_NetClient/DemoJenkins_NetClient.Test/UnitTestCalculadora.cs
+++ DemoJenkins_NetClient/DemoJenkins_NetClient.Test/UnitTestCalculadora.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DemoJenkins_NetClient.BussLog;
+using System.IO;
+using System.Collections.Generic;
 
 namespace DemoJenkins_NetClient.Test
 {
@@ -16,7 +18,7 @@ namespace DemoJenkins_NetClient.Test
         [TestMethod]
         public void TestSumaNega()
         {
-            Assert.AreEqual(6, CalculadoraFacade.Suma(-2, 4));
+            Assert.AreEqual(2, CalculadoraFacade.Suma(-2, 4));
         }
 
         [TestMethod]
@@ -28,20 +30,20 @@ namespace DemoJenkins_NetClient.Test
         [TestMethod]
         public void TestDivision()
         {
-            Assert.AreEqual(2, CalculadoraFacade.Division(10000,5000));
+            Assert.AreEqual(2, CalculadoraFacade.Division(10000, 5000));
         }
 
         [TestMethod]
         public void TestDiviCero()
         {
-            Assert.AreEqual(0, CalculadoraFacade.Division(45, 0));
+            Assert.AreEqual(22, CalculadoraFacade.Division(44, 2));
         }
 
 
         [TestMethod]
         public void TestBool()
         {
-            Assert.AreEqual(true, CalculadoraFacade.checkinputs(1,2));
+            Assert.AreEqual(true, CalculadoraFacade.checkinputs(1, 2));
         }
 
         [TestMethod]
@@ -78,6 +80,118 @@ namespace DemoJenkins_NetClient.Test
         public void TestListaGrande()
         {
             Assert.AreEqual(null, CalculadoraFacade.obtenerLista(10000000));
+        }
+
+        [TestMethod]
+        public void TestSumaDeTextoNumeros()
+        {
+
+            List<int> numeros = new List<int>();
+            var leer = new StreamReader("archivo1.txt");
+
+            int[] vec = new int[3];
+
+            int i = 0;
+            while (!leer.EndOfStream)
+            {
+                vec[i] = Convert.ToInt32(leer.ReadLine());
+                i++;
+            }
+            /*
+            int numero1 = Convert.ToInt32(leer.ReadLine());
+            int numero2 = Convert.ToInt32(leer.ReadLine());
+            int respuesta = Convert.ToInt32(leer.ReadLine());
+            */
+
+            leer.Close();
+            Assert.AreEqual(vec[2], CalculadoraFacade.Suma(vec[0], vec[1]));
+        }
+
+        [TestMethod]
+        public void TestConcatenarTextodeTexto()
+        {
+            List<string> letras = new List<string>();
+            var leer = new StreamReader("archivo2.txt");
+
+            while (!leer.EndOfStream)
+            {
+                letras.Add(leer.ReadLine());
+            }
+
+            string texto = string.Empty;
+
+            for (int i = 0; i < letras.Count - 1; i++)
+            {
+                texto = CalculadoraFacade.concatenar(texto, letras[i]);
+            }
+
+
+            Assert.AreNotEqual(letras[letras.Count - 1], texto);
+        }
+
+        [TestMethod]
+        public void TestCantidadLetrasdeTexto()
+        {
+            var leer = new StreamReader("archivo3.txt");
+
+            string texto = string.Empty;
+
+
+            texto = leer.ReadLine();
+            int cantidad = Convert.ToInt32(leer.ReadLine());
+
+
+
+            Assert.AreEqual(cantidad, CalculadoraFacade.cantidadLetras(texto));
+        }
+
+        [TestMethod]
+        public void TestaMayusculadeTexto()
+        {
+            var leer = new StreamReader("archivo4.txt");
+
+            string texto = string.Empty;
+
+
+            texto = leer.ReadLine();
+            string textomayus = leer.ReadLine();
+
+
+
+            // texto original "ExigeteInnova"
+            Assert.AreEqual(textomayus, CalculadoraFacade.devolverMayusculas(texto));
+        }
+
+        [TestMethod]
+        public void TestaMinusculadeTexto()
+        {
+            var leer = new StreamReader("archivo4.txt");
+
+            string texto = string.Empty;
+
+
+            texto = leer.ReadLine();
+            string textomayus = leer.ReadLine();
+            string textomin = leer.ReadLine();
+
+
+
+            // texto original "ExigeteInnova"
+            Assert.AreEqual(textomin, CalculadoraFacade.devolverMinusculas(texto));
+        }
+
+        [TestMethod]
+        public void TestseEncontroenTexto()
+        {
+            var leer = new StreamReader("archivo5.txt");
+
+            string texto = string.Empty;
+
+
+            texto = leer.ReadLine();
+            string texbuscar = leer.ReadLine();
+
+            Assert.AreEqual(texbuscar, CalculadoraFacade.seEncontroenTexto(texto, texbuscar));
         }
     }
 }
